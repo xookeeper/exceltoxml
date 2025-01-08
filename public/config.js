c3e5a43d402177e349b8c9013ce8cc2d13c828ec
xookeeper <purushottam423@gmail.com>
2025-01-08 16:58:23 +0530

    integrated backend node app

--- public/config.js
+++ public/config.js
@@ -1,5 +1,5 @@
 const config = {
-    serverUrl: 'http://ec2-184-73-91-63.compute-1.amazonaws.com:3000'
+    serverUrl: 'http://ec2-184-73-91-63.compute-1.amazonaws.com:3001'
   };
   
   export default config;
--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const userRoutes = require('../routes/userRoutes')
 const path = require('path');
 
 const app = express();
-const port = 3000;
+const port = 3001;
 
 // Middleware to parse JSON bodies
 app.use(bodyParser.json());
@@ -39,6 +39,27 @@ app.get('/converter', (req, res) => {
 app.get('/adminPanel', (req, res) => {
   res.sendFile(path.join(__dirname, '../public', 'adminPanel.html'));
 });
+
+app.get('/config.js', (req, res) => {
+  res.setHeader('Content-Type', 'application/javascript');
+  res.send(`
+    const config = {
+      serverUrl: '${process.env.SERVER_URL}',
+      firebaseConfig: {
+        apiKey: '${process.env.FIREBASE_API_KEY}',
+        authDomain: '${process.env.FIREBASE_AUTH_DOMAIN}',
+        projectId: '${process.env.FIREBASE_PROJECT_ID}',
+        storageBucket: '${process.env.FIREBASE_STORAGE_BUCKET}',
+        messagingSenderId: '${process.env.FIREBASE_MESSAGING_SENDER_ID}',
+        appId: '${process.env.FIREBASE_APP_ID}',
+        measurementId: '${process.env.FIREBASE_MEASUREMENT_ID}'
+      }
+    };
+    export default config;
+  `);
+});
+
+
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
